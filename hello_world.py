c83ea918c662149eefbe29cf9535c9c8a42b9f1c
Mithulacs <124509068+Mithulacs@users.noreply.github.com>
2024-07-09 21:21:22 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# use input() to request user name
+# print user name
+# use input() to request user age
+# print user age
+# print "Hello World!"
+
+name = input("Please enter your name: ")
+print(name)
+age = input ("PLease enter your age: ")
+print(age)
+print("Hello World!")
