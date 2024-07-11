b2a50c4cdd890395f58b66705bb0c8888e117452
Mithulacs <124509068+Mithulacs@users.noreply.github.com>
2024-07-11 22:44:35 +0100

    Add files via upload

---
+++ details.py
@@ -0,0 +1,9 @@
+# use input to request user Name, age, House number and Street name
+# use f-string to print out in one line the output of each input
+
+full_name = input("Please enter your full name: ")
+age = input("PLease enter your age: ")
+house_number = input("Please enter your house number: ")
+street_name = input("Please enter your street name: ")
+print(f"This is {full_name}. He/she is {age} years old and lives at house number {house_number} on {street_name}.")
+
