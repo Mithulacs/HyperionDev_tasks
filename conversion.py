6e256b5faf426cad34b8fee721789260d78e96e3
Mithulacs <124509068+Mithulacs@users.noreply.github.com>
2024-07-12 23:00:16 +0100

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,32 @@
+#*******STEP 1*******
+# create variable num1 and assign value 99.23
+# create variable num2 and assign value 23
+# create variable num3 and assign value 150
+# create variable string1 and assign value "100"
+
+#********STEP 2*******
+# cast int() to convert num1 into integer
+# cast float() to convert num2 into float
+# cast str() to convert num3 into string
+# cast int() to  convert string1 into integer
+
+#********STEP 3*******
+# use print() to print out each variable on a separate line
+
+# STEP 1
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# STEP 2
+num1 = int(99.23)
+num2 = float(23)
+num3 = str(150)
+string1 = int(100)
+
+# STEP 3
+print(f"Variable num1 integer value is {num1}.")
+print(f"Variable num2 float value is {num2}.")
+print(f"Variable num3 string value is {num3}.")
+print(f"Variable string1 integer value is {string1}.")
