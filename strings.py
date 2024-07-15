beb8f63e1cb3f591e8f6035645cbb496cb88bf28
Mithulacs <124509068+Mithulacs@users.noreply.github.com>
2024-07-15 17:05:38 +0100

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,7 @@
+# Create a string variable called hero and assign value "$$$Superman$$$""
+# Use strip() method to remove "$" 
+# Print hero ---> the output should be "Superman"
+
+hero = "$$$Superman$$$"
+hero = hero.strip("$")
+print(hero)
